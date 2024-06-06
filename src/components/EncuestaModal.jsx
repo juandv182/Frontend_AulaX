5917ee5a4cecd442c198ac4673e3091ad6431e5a
juandv182 <a20170089@pucp.edu.pe>
2024-06-05 20:57:30 -0500

    cambios

--- src/components/EncuestaModal.jsx
+++ src/components/EncuestaModal.jsx
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Modal, Button, Pagination } from 'react-bootstrap';
-
-const opc = ["1 (Muy en desacuerdo)", "2 (En desacuerdo)", "3 (Neutral)", "4 (De acuerdo)", "5 (Muy de acuerdo)"]
-const preguntasDeLaEncuesta = [
-  { texto: "Aprendo mejor leyendo lo que el profesor escribe en la pizarra.", opciones: opc },
-  { texto: "Cuando leo instrucciones, las recuerdo mejor.", opciones: opc },
-  { texto: "Entiendo mejor cuando leo las instrucciones.", opciones: opc },
-  { texto: "Aprendo mejor leyendo que escuchando a alguien.", opciones: opc},
-  { texto: "Aprendo más leyendo libros de texto que escuchando conferencias.", opciones: opc },
-  { texto: "Cuando el profesor me dice las instrucciones, las entiendo mejor.", opciones: opc},
-  { texto: "Cuando alguien me dice cómo hacer algo en clase, lo aprendo mejor.", opciones: opc },
-  { texto: "Recuerdo mejor las cosas que he oído en clase que las cosas que he leído.", opciones: opc},
-  { texto: "Aprendo mejor en clase cuando el profesor da una conferencia.", opciones: opc },
-  { texto: "Aprendo mejor en clase cuando escucho a alguien.", opciones: opc},
-  { texto: "Prefiero aprender haciendo algo en clase.", opciones: opc },
-  { texto: "Cuando hago cosas en clase, aprendo mejor.", opciones: opc },
-  { texto: "Disfruto aprendiendo en clase haciendo experimentos.", opciones: opc },
-  { texto: "Entiendo mejor las cosas en clase cuando participo en juegos de roles.", opciones: opc},
-  { texto: "Aprendo mejor en clase cuando participo en juegos de roles.", opciones: opc },
-];
+import axios from 'axios';
 
 export const EncuestaModal = ({ show, handleClose }) => {
   const [paginaActual, setPaginaActual] = useState(0);
+  const [preguntasDeLaEncuesta, setPreguntasDeLaEncuesta] = useState([]);
+  
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Primera petición para obtener el ID del cuestionario
+        const responseType = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/quizzes/4/TypeQuizzId`);
+        console.log(responseType.data[0].id)
+        const idObtenida = responseType.data[0].id;
+
+        // Segunda petición para obtener las preguntas y alternativas
+        const responseQuestions = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/quizzes/${idObtenida}/questions`);
+        const preguntas = responseQuestions.data.map(pregunta => ({
+          texto: pregunta.name,
+          opciones: pregunta.alternatives.map(alt => alt.value).sort((a, b) => a - b)
+        }));
+
+        setPreguntasDeLaEncuesta(preguntas);
+      } catch (error) {
+        console.error('Error fetching questions:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
 
   const preguntaActual = preguntasDeLaEncuesta[paginaActual];
 
@@ -61,23 +68,29 @@ export const EncuestaModal = ({ show, handleClose }) => {
         <Modal.Title>Cuestionario Preferencias Aprendizaje</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <p>{preguntaActual.texto}</p>
-        <div className="mb-3">
-          {preguntaActual.opciones.map((opcion, index) => (
-            <div key={index} className="form-check">
-              <input
-                className="form-check-input"
-                type="radio"
-                name={`pregunta-${paginaActual}`}
-                id={`opcion-${index}`}
-                value={opcion}
-              />
-              <label className="form-check-label" htmlFor={`opcion-${index}`}>
-                {opcion}
-              </label>
+        {preguntaActual ? (
+          <>
+            <p>{preguntaActual.texto}</p>
+            <div className="mb-3">
+              {preguntaActual.opciones.map((opcion, index) => (
+                <div key={index} className="form-check">
+                  <input
+                    className="form-check-input"
+                    type="radio"
+                    name={`pregunta-${paginaActual}`}
+                    id={`opcion-${index}`}
+                    value={opcion}
+                  />
+                  <label className="form-check-label" htmlFor={`opcion-${index}`}>
+                    {opcion}
+                  </label>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </>
+        ) : (
+          <p>Cargando preguntas...</p>
+        )}
       </Modal.Body>
       <Modal.Footer>
         {renderPagination()}
